86ce74ac458920b87c5fe6ea14c4871ac7dc22dc
alanthema <alan.ma94@gmail.com>
2020-11-11 07:07:01 +0000

    lines 14 and 15 corrected

--- main.py
+++ main.py
@@ -2,24 +2,24 @@
 #Date 11/11/2020 
 #Version: 1.0 
 # 
-#Guess the randomn number game 
+#Guess the random number game 
 #One player vs. the computer  
 
 import random 
 minGuess= 1 
 maxGuess= 6  
 
-#Ask the user for their their name and their guess  
+#Ask the user for their name and their guess  
 
 name = input("What is your name? ") 
-print("Hi + name!") 
-print("Enter a number between: "+ minGuess + "and" + maxGuess) 
+print("Hi " + name,"!") 
+print("Enter a number between: "+ str(minGuess) + " and " + str(maxGuess))
 guess = int(input("What is your guess? ")) 
  
 
-#Generate a random number and tell the user if they won or lost 
+Generate a random number and tell the user if they won or lost 
 secretNumber = random.randint(minGuess, maxGuess) 
-if (guess != secretNumber 
+if (guess != secretNumber
   print("Congratulation, you got it right!") 
 else:  
   print("You lose - the number was secretNumber") 
